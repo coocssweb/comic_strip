4eef9b56a0b4139a972606e588eb1db95c3afb63
wangjiaxin <wangjiaxin@leedarson.com>
2026-07-23 14:59:14 +0800

    s

--- admin-web/src/App.jsx
+++ admin-web/src/App.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Routes } from 'react-router-dom';
 
-import ProtectedLayout from './layouts/ProtectedLayout';
-import Home from './pages/Home';
-import Login from './pages/Login';
+
 import { store as defaultStore } from './store';
 
 export default function App({ store = defaultStore }) {
@@ -12,10 +10,7 @@ export default function App({ store = defaultStore }) {
     <Provider store={store}>
       <BrowserRouter>
         <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route element={<ProtectedLayout />}>
-            <Route path="/" element={<Home />} />
-          </Route>
+
         </Routes>
       </BrowserRouter>
     </Provider>
